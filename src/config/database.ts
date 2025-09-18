00e483234831a2f94bc4f5e1dd146314f504fc5c
Stephanie Nwankwo <stephybelle208@gmail.com>
2025-09-18 18:48:07 +0100

    feat: change db to cloud

--- src/config/database.ts
+++ src/config/database.ts
@@ -24,8 +24,8 @@ export const AppDataSource = new DataSource({
     ],
     migrations: ['src/migrations/*.ts'],
     subscribers: ['src/subscribers/*.ts'],
-    ssl: false,
-    // ssl: { rejectUnauthorized: false },
+    // ssl: false,
+    ssl: { rejectUnauthorized: false },
 });
 export const connectDB = async (): Promise<void> => {
     try {
