4b7d0d98f516c07676928d0555efb5d879527033
Stephanie Nwankwo <badviruscoder@gmail.com>
2025-12-08 19:04:27 +0100

    fix: build issues

--- services/auth/src/server.ts
+++ services/auth/src/server.ts
@@ -38,9 +38,8 @@ app.use(morgan(process.env.LOG_FORMAT || 'dev', { stream: morganStream }));
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per window
-  standardHeaders: true,
-  legacyHeaders: false,
-  message: { error: 'Too many requests, please try again later.' },
+  // legacyHeaders: false,
+  message: 'Too many requests, please try again later.',
 });
 app.use('/api/auth', limiter);
 
